f2d085ed6eea88c50d99534c8d23e570ca28b016
ronbhadad22 <https://www.google.com/support/accounts/bin/answer.py?answer=181692>
2026-03-04 00:56:08 +0200

    my first deploy

--- cicd-lab/app/server.js
+++ cicd-lab/app/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const os = require('os');
 
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 const VERSION = process.env.APP_VERSION || '1.0.0';
+
 
 // Home page
 app.get('/', (req, res) => {
