222d65c66218ef33d5e2aa8452297933585131c9
ronbhadad22 <ron.hadad@g.int-college.co.il>
2026-03-04 18:19:17 +0200

    Update server.js

--- cicd-lab/app/server.js
+++ cicd-lab/app/server.js
@@ -3,12 +3,12 @@ const os = require('os');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const VERSION = process.env.APP_VERSION || '1.0.0';
+const VERSION = process.env.APP_VERSION || '1.1.2';
 
 // Home page
 app.get('/', (req, res) => {
   res.send(`
-    <h1>CI/CD Demo App</h1>
+    <h1>CI/CD Demo App hertz</h1>
     <p>Version: ${VERSION}</p>
     <p>Hostname: ${os.hostname()}</p>
     <p>Time: ${new Date().toISOString()}</p>
